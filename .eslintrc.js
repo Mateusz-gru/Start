8b1cce47130c3550fe8b42f254e48f697270f9be
Mateusz <grudzinski.mateusz4@gmail.com>
2021-03-30 20:26:39 +0200

    start

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
